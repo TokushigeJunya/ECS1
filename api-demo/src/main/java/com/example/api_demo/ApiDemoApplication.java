fd7df9af3af99b24b8745b2eb1c66fdd7e9972a2
Tokushige Junya <tokushige.281@gmail.com>
2025-06-22 11:54:31 +0900

    Update yaml

--- api-demo/src/main/java/com/example/api_demo/ApiDemoApplication.java
+++ api-demo/src/main/java/com/example/api_demo/ApiDemoApplication.java
@@ -2,12 +2,18 @@ package com.example.api_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApiDemoApplication {
+public class ApiDemoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiDemoApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApiDemoApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDemoApplication.class, args);
+    }
 }
